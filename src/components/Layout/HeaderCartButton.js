70e9d968097a23ae9a8a4de52d40f2dcbbf4e82c
Rick <ricksmycodeguy@gmail.com>
2022-01-23 17:12:35 -0600

    Adds cart icon code for Header

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import classes from './HeaderCartButton.module.css';
+import CartIcon from '../Cart/CartIcon';
 
 const HeaderCartButton = (props) => {
   return (
     <button
-      type={props.type || 'button'}
-      className={`${classes.button} ${props.className}`}
-      onClick={props.onClick}
-      disabled={props.disabled}
+      // type={props.type || 'button'}
+      // className={`${classes.button} ${props.className}`}
+      // onClick={props.onClick}
+      // disabled={props.disabled}
     >
+      <span>
+        <CartIcon />
+      </span>
+      <span></span>
+      <span></span>
       {props.children}
-    </button>
+    </button> 
   );
 };
 
