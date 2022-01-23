c96dbb3b9ed291fa2b55db02d1ea805e2c156306
Rick <ricksmycodeguy@gmail.com>
2022-01-23 17:04:19 -0600

    Refactors Layout button

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,14 @@
 import React, { Fragment } from 'react';
 import classes from './Header.module.css';
-import mealsImage from '../../assets/meals.jpg'
+import mealsImage from '../../assets/meals.jpg';
+import Button from '../Layout/HeaderCartButton';
 
 const Header = (props) => {
   return (
   <Fragment>
       <header className={classes.header}>
         <h1>Rick's ReactMeals App</h1>
-        <button>Cart</button>
+        <Button>Cart</Button>
     </header>
       <div className={classes['main-image']}>
         <img src={ mealsImage } alt="Table with food"/>
---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import classes from './HeaderCartButton.module.css';
+
+const HeaderCartButton = (props) => {
+  return (
+    <button
+      type={props.type || 'button'}
+      className={`${classes.button} ${props.className}`}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+export default HeaderCartButton;
