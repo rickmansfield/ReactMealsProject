b71d8f0d144d22cfba8fb80fcf59d9663f2c4207
Rick <ricksmycodeguy@gmail.com>
2022-04-03 10:02:12 -0500

    Makes component easier to read

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -3,18 +3,26 @@ import classes from './MealItem.module.css';
 
 
 const MealItem = (props) => {
+  console.log('MealItem.js lin 6 props', props);
   const price = `$${props.price.toFixed(2)}`;
   return (
     <li className={classes.meal}>
       <div>
         <h3>{props.name}</h3>
-        <div className={classes.description}>{props.description}</div>
-        <div className={classes.price}>{price}</div>
+
+        <div className={classes.description}>
+          {props.description}
+        </div>
+
+        <div className={classes.price}>
+          {price}
+        </div>
       </div>
 
       <div>
         <button onClick={props.onClick}>Add to Cart</button>
       </div>
+      
     </li>
   );
 };
