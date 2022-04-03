23b853e5fb57e769a3e93db4b67c2dbf0fbaaf6a
Rick <ricksmycodeguy@gmail.com>
2022-04-03 11:53:16 -0500

    Adds some consolelogs to follow props

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -3,7 +3,7 @@ import classes from './card.module.css';
 
 const Card = (props) => {
   console.log('Card.js lin 5 props', props);
-  console.log('Card.js lin 5 props.children', props.children);
+  console.log('Card.js lin 6 props.children', props.children);
   return (
     <div className={classes.card}>{props.children}</div>
   )
