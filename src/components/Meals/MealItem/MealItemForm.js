68c0304896a49093d3690008087a5624ae751f06
Rick <ricksmycodeguy@gmail.com>
2022-04-03 11:52:16 -0500

    Adds MealItemForm component & CSS

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from 'react';
+import classes from './MealItemForm.module.css';
 
-const MealItemForm = () => {
+const MealItemForm = (props) => {
   return (
-    <div>MealItemForm</div>
+    <form className={classes.form}>
+      <input />
+      <button>+ Add</button>
+    </form>
   )
 }
 
