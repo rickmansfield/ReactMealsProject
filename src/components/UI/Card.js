9d5ac6237846a72748f0aafb21102360998b5d21
Rick <ricksmycodeguy@gmail.com>
2022-04-03 10:02:35 -0500

    Adds console logs to follow props

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -2,6 +2,8 @@ import React from 'react';
 import classes from './card.module.css';
 
 const Card = (props) => {
+  console.log('Card.js lin 5 props', props);
+  console.log('Card.js lin 5 props.children', props.children);
   return (
     <div className={classes.card}>{props.children}</div>
   )
