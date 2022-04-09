728f966f4134c2c62d21c4aa312747da2b0771a7
Rick Mansfield <PEACEFULRICK@msn.com>
2022-04-09 17:10:38 -0500

    Uses dynamic id via props

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -2,6 +2,7 @@ import React from 'react';
 import classes from './Input.module.css';
 
 const Input = (props) => {
+  console.log('Input.js lin 5 props', props);
   return (
     <div className={classes.Input}>
       <label htmlFor={props.input.id}>
