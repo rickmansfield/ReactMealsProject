2bdacdbc923c474ef4ee24641acd705d0f73bd28
Rick Mansfield <PEACEFULRICK@msn.com>
2022-04-11 18:18:20 -0500

    Employes Context for state

--- src/components/Header/HeaderCartButton.js
+++ src/components/Header/HeaderCartButton.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import classes from './HeaderCartButton.module.css';
+import React, { useContext } from 'react';
 import CartIcon from '../Cart/CartIcon';
+import CartContext from '../../store/cart-context.js';
+import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+  const numberofCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
   return (
     <button
       // type={props.type || 'button'}
       className={classes.button}
-      // onClick={props.onClick}
-      // disabled={props.disabled}
+      onClick={props.onClick}
+    // disabled={props.disabled}
     >
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Cart</span>
-      <span className={ classes.badge }>
-      3
+      <span className={classes.badge}>
+        {numberofCartItems}
       </span>
       {/* {props.children} */}
-    </button> 
+    </button>
   );
 };
 
