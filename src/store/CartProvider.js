03695a44faef9c02775358643d7bd0bc8ddad053
Rick Mansfield <PEACEFULRICK@msn.com>
2022-04-16 14:36:15 -0500

    +cartReducer conditional | Builds out CartProvider

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -8,13 +8,31 @@ const defaultCartState = {
 }
 
 const cartReducer = (state, action) => {
+  if (action.type === 'ADD') { 
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount
+    }
+  }
   return defaultCartState;
  };
 
 const CartProvider = (props) => {
   const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
-  const addItemToCartHandler = (item) => { };
-  const removeItemFromCartHandler = (id) => { };
+  const addItemToCartHandler = (item) => { 
+    dispatchCartAction({
+      type: 'ADD',
+      item: item
+    });
+  };
+  const removeItemFromCartHandler = (id) => { 
+    dispatchCartAction({
+      type: 'REMOVE',
+      id: id
+    });
+  };
 
   const cartContext = {
     items: cartState.items,
