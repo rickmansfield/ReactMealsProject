5df17e434728d7889a68ee7b644ce8710848f2a1
Rick Mansfield <PEACEFULRICK@msn.com>
2022-05-01 16:47:54 -0500

    Updates Naming convention

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,11 +4,12 @@ import HeaderCartButton from './HeaderCartButton';
 import mealsImage from '../../Assets/meals.jpg';
 
 const Header = (props) => {
+  console.log('Header.js line: 7 props: ', props);
   return (
   <Fragment>
       <header className={classes.header}>
         <h1>Rick's ReactMeals App!</h1>
-        <HeaderCartButton onClick={ props.onShowCart }/>
+        <HeaderCartButton onClick={ props.onClick }/>
     </header>
       <div className={classes['main-image']}>
         <img src={ mealsImage } alt="Table with food"/>
