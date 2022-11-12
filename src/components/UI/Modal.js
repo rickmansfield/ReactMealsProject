f66d492ed47f5755c88ba2ba14719d317843481b
Rick <ricksmycodeguy@gmail.com>
2022-11-12 14:18:46 -0600

    Updates Comments

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -16,8 +16,8 @@ const ModalOverlay = (props) => {
   </div>
 };
 
-const portalElement = document.getElementById('overlays');
 //Note that we are using React.Fragment here to avoid adding an extra div to the DOM. If we don't use React.Fragment, then we will have an extra div in the DOM. You can use <React.Fragment> or import { Fragment} and use the shorter verison <Fragment> you can use a <div> as well. But remember if you wrap the content in a div, then you will have an extra div in the DOM. So, it's better to use React.Fragment or <Fragment> to avoid adding an extra div to the DOM.
+const portalElement = document.getElementById('overlays');
 const Modal = (props) => {
   return (
     <Fragment>
